525338160cdf46bc3dfa2828a26701d3c79d292a
DESKTOP-I1TUD21\USUARIO <mangelmoreno>
2022-07-20 11:18:03 -0500

    primer commit

---
+++ src/main/java/com/fundamentos/sprintboot/funamentos/FunamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.sprintboot.funamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunamentosApplication.class, args);
+	}
+
+}
